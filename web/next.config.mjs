689c473c77a83c49adf1199d5bafc44d3d455aa4
maulana-tech <firdaussyah03@gmail.com>
2026-06-26 19:42:11 +0700

    chore(web/next.config.mjs): auto update

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@eclipse/crypto", "@eclipse/sdk"],
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
+        ],
+      },
+    ];
+  },
   webpack: (config, { isServer }) => {
     // Required for Barretenberg WASM + top-level await in bb.js
     config.experiments = {
